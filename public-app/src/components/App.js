6590b96274a8e9d901f566ba992fb6ee024507f9
Alethea Toh <aletheatoh@Aletheas-MacBook-Pro.local>
2019-03-18 12:34:46 -0400

    changes

--- public-app/src/components/App.js
+++ public-app/src/components/App.js
@@ -30,7 +30,29 @@ const mapDispatchToProps = dispatch => ({
 })
 
 function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+    // return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+
+    var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+  };
+  return (typeof window.orientation !== "undefined") || isMobile.any();
 };
 
 class App extends Component {
